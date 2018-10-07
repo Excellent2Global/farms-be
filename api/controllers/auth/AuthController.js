4cbbfa0006c3181f8c916fd0d0b82c4ccf39c31b
Sadiq Hassan <abubakarhassan59@gmail.com>
2018-10-07 17:09:40 +0100

    Implemented login

--- api/controllers/auth/AuthController.js
+++ api/controllers/auth/AuthController.js
@@ -4,6 +4,26 @@
  * @description :: Server-side actions for handling incoming requests.
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
+var passport = require('passport');
+
+function _onPassportAuth(req, res, error, user, info) {
+  if (error) {
+    sails.log.error(error);
+    return res.serverError(error);
+  }
+  if (!user) {
+    sails.log.error(info);
+    return res.send({
+      code: info.code,
+      message: info.message
+    });
+  }
+
+  return res.ok({
+    token: CipherService.createToken(user),
+    user: user
+  });
+}
 
 module.exports = {
   
@@ -12,9 +32,8 @@ module.exports = {
    * `AuthController.login()`
    */
   login: async function (req, res) {
-    return res.json({
-      todo: 'login() is not implemented yet!'
-    });
+    sails.log.info('POST /auth/login');
+    passport.authenticate('local', _onPassportAuth.bind(this, req, res))(req, res);
   },
 
   /**
