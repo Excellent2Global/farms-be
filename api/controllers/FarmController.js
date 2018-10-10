f9b9a57625c080d534cfc1bc5b830592460cd782
Sadiq Hassan <abubakarhassan59@gmail.com>
2018-10-10 23:06:05 +0100

    Finished logic to add a new farm

--- api/controllers/FarmController.js
+++ api/controllers/FarmController.js
@@ -6,7 +6,30 @@
  */
 
 module.exports = {
-  addFarm: (req, res) => {},
+  addFarm: async (req, res) => {
+    sails.log.info('POST /farm');
+    let farmDetails = req.body;
+
+    var newFarmDetails = await Farm.create(
+      Object.assign({
+        shortName: farmDetails.shortName,
+        address: farmDetails.address,
+        city: farmDetails.city,
+        state: farmDetails.state
+      })
+    )
+    .intercept((error) => {
+      return res.json({
+        error
+      });
+    })
+    .fetch();
+
+    return res.ok({
+      code: 201,
+      message: 'New farm added successfully'
+    });
+  },
 
   fetchFarms: (req, res) => {},
 
