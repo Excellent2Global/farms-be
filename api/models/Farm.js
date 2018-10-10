5bed26237378c12de0bc6b9ebb544897833ea4be
Sadiq Hassan <abubakarhassan59@gmail.com>
2018-10-10 18:55:53 +0100

    Added farm model

---
+++ api/models/Farm.js
@@ -0,0 +1,62 @@
+/**
+ * Farm.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    shortName: {
+      type: 'string',
+      unique: true,
+      required: true,
+    },
+
+    address: {
+      type: 'string',
+      required: true
+    },
+
+    city: {
+      type: 'string',
+      required: true
+    },
+
+    state: {
+      type: 'string',
+      required: true
+    },
+
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+    halls: {
+      collection: 'hall',
+      via: 'farm'
+    },
+
+    ponds: {
+      collection: 'pond',
+      via: 'farm'
+    }
+
+  },
+
+  customToJSON: () => {
+    return _.omit(this, ['createdAt', 'updatedAt']);
+  }
+
+};
+
