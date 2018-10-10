e3d8718ae3adf652d301b289c70f478add035730
Sadiq Hassan <abubakarhassan59@gmail.com>
2018-10-10 23:53:09 +0100

    Updated endpoints to refrect supported ones

--- config/routes.js
+++ config/routes.js
@@ -33,10 +33,11 @@ module.exports.routes = {
   'GET /v1/staff/:username': 'StaffController.getStaffByUsername',
   'PUT /v1/staff/:username': 'StaffController.updateStaff',
 
-  'GET /v1/farm': 'FarmController.fetchStaff',
   'POST /v1/farm': 'FarmController.addFarm',
+  'GET /v1/farm': 'FarmController.fetchFarms',
   'GET /v1/farm/:farmId': 'FarmController.getFarmById',
-  'PUT /v1/farm/:farmId': 'FarmController.updateFarm',
+  'POST /v1/farm/:farmId/hall': 'FarmController.addHall',
+  'POST /v1/farm/:farmId/pond': 'FarmController.addPond',
 
   //  ╦ ╦╔═╗╔╗ ╦ ╦╔═╗╔═╗╦╔═╔═╗
   //  ║║║║╣ ╠╩╗╠═╣║ ║║ ║╠╩╗╚═╗
