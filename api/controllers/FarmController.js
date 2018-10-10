06d3a37a744e3a3df072e6d32fc43bd7f0c3bc91
Sadiq Hassan <abubakarhassan59@gmail.com>
2018-10-10 18:57:10 +0100

    Added Farm controller

---
+++ api/controllers/FarmController.js
@@ -0,0 +1,12 @@
+/**
+ * FarmController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  
+
+};
+
